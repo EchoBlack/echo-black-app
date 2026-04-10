cc8bf62e9c609dc9dfac163e6279c4fb3602bffb
EchoBlack <Jamesc.marsh@yahoo.com>
2026-04-10 03:01:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const publicDir = path.join(__dirname, "public");
+const publicDir = __dirname;
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(publicDir));
